65ad9a96fd38bc1b33676785be891420f1c9ea40
Abby <AbbyNode@users.noreply.github.com>
2019-11-04 11:42:32 -0500

    Re-added files

---
+++ Assets/Scripts/DestroyByBoundary.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyByBoundary : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void OnTriggerExit2D(Collider2D collider)
+    {
+        if(collider.gameObject.tag == "Boundary")
+        {
+            if(gameObject.tag == "PlayerWeapon")
+            {
+                gameObject.SetActive(false);
+            }
+            else
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+}
