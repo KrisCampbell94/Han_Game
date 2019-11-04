9efdfd3d3b888b0b4214735dc1a7d99814994be5
Kristian Campbell <chipandchuckcreator@gmail.com>
2019-11-03 19:22:36 -0500

    Initial Commit

---
+++ Assets/Scripts/KunaiScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KunaiScript : MonoBehaviour
+{
+    public bool isRight = true;
+    public float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isRight)
+        {
+            transform.position = new Vector2(transform.position.x + (speed * Time.deltaTime), transform.position.y);
+        }
+        else
+        {
+            transform.eulerAngles = new Vector2(0, 180);
+            transform.position = new Vector2(transform.position.x - (speed * Time.deltaTime), transform.position.y);
+        }
+    }
+}
