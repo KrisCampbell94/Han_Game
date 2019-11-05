4bd5d6645d22e9b3d84f1868820d5307a8127ad9
Kristian Campbell <chipandchuckcreator@gmail.com>
2019-11-04 22:59:15 -0500

    Attack + Enemy Implementation Pt.1
    Player
    - Attacks Close or at Range.
    - Has a workable HP counter.
    - Proper Hit Boxes
    Enemy
    - Only Attacks if player is close.
    - Workable HP Counter.
    - Proper Hit Boxes

---
+++ Assets/Scripts/PlayerEncounterScript.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerEncounterScript : MonoBehaviour
+{
+    public bool isPlayerClose = false;
+    public bool isEnemyClose = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerStay2D(Collider2D collider)
+    {
+        if (collider.gameObject.tag == "Player")
+        {
+            isPlayerClose = true;
+        }
+        if(collider.gameObject.tag == "Enemy" || collider.gameObject.tag == "EnemyWeapon")
+        {
+            isEnemyClose = true;
+        }
+
+    }
+    void OnTriggerExit2D(Collider2D collider)
+    {
+        if (collider.gameObject.tag == "Player")
+        {
+            isPlayerClose = false;
+        }
+        if (collider.gameObject.tag == "Enemy" || collider.gameObject.tag == "EnemyWeapon")
+        {
+            isEnemyClose = false;
+        }
+    }
+}
