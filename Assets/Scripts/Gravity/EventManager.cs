6aeb489155d4dffc051cb87f7391f5bed8e56bb2
Abby <AbbyNode@users.noreply.github.com>
2019-11-04 21:14:33 -0500

    Basic event system

---
+++ Assets/Scripts/Gravity/EventManager.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Based on
+// https://stackoverflow.com/questions/42034245/unity-eventmanager-with-delegate-instead-of-unityevent
+public class EventManager : ScriptableObject {
+	// Holds reference to all events
+	private Dictionary<string, Action> events;
+
+	// Singleton
+	private static EventManager eventManager;
+
+	// Get static singleton instance
+	public static EventManager Instance {
+		get {
+			// If does not exist, initialize
+			if (!eventManager) {
+				eventManager = new EventManager();
+				eventManager.Init();
+			}
+
+			return eventManager;
+		}
+	}
+
+	// Init events dictionary
+	void Init() {
+		if (events == null) {
+			events = new Dictionary<string, Action>();
+		}
+	}
+
+	// Add listener to event
+	public static void AddListener(string eventName, Action listener) {
+		// If event doesn't exist, create a new one
+		if (!Instance.events.ContainsKey(eventName)) {
+			Instance.events.Add(eventName, listener);
+		} else { // If event does exist, add listener to event
+			Instance.events[eventName] += listener;
+		}
+	}
+
+	// Remove listener from event
+	public static void RemoveListener(string eventName, Action listener) {
+		// If there's no eventManager, there's no events
+		if (eventManager == null) return;
+
+		// If event exists, remove listener from it
+		if (Instance.events.ContainsKey(eventName)) {
+			Instance.events[eventName] -= listener;
+		}
+	}
+
+	// Invoke all associated listeners
+	public static void InvokeEvent(string eventName) {
+		// If event exists, invoke all listeners
+		if (Instance.events.ContainsKey(eventName)) {
+			Instance.events[eventName].Invoke();
+		}
+	}
+
+	// Get all registered events
+	public static Dictionary<string, Action>.KeyCollection GetRegisteredEvents() {
+		return Instance.events.Keys;
+	}
+}
---
+++ Assets/Scripts/Gravity/PlayerInput.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class PlayerInput : MonoBehaviour
+{
+	public enum Direction { Left, Right }
+	public enum Movement { Stopped, Walking, Running }
+
+	private Direction direction;
+	private Movement movement;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+		movement = Movement.Stopped;
+		direction = Direction.Right;
+
+		AddEventDebug("Stopping");
+		AddEventDebug("Walking");
+		AddEventDebug("Running");
+
+		AddEventDebug("TurnLeft");
+		AddEventDebug("TurnRight");
+	}
+
+	// Update is called once per frame
+	void Update()
+    {
+
+    }
+
+	private void FixedUpdate() {
+		float horizontalMove = Input.GetAxis("Horizontal");
+
+		// Check current movement
+		switch (movement) {
+			case Movement.Stopped:
+				// If stopped and input says move
+				if (horizontalMove != 0) {
+					// Change to moving and call event
+					movement = Movement.Walking;
+					EventManager.InvokeEvent("Walking");
+				}
+				break;
+			case Movement.Walking:
+				// If walking and input says stop
+				if (horizontalMove == 0) {
+					// Change to stopped and call event
+					movement = Movement.Stopped;
+					EventManager.InvokeEvent("Stopping");
+				}
+				break;
+			case Movement.Running:
+				// If running and input says stop
+				if (horizontalMove == 0) {
+					// Change to stopped and call event
+					movement = Movement.Stopped;
+					EventManager.InvokeEvent("Stopping");
+				}
+				break;
+		}
+		
+		// Check current direction
+		switch (direction) {
+			case Direction.Left:
+				// If facing left and movement says right
+				if (horizontalMove > 0) {
+					// Change to right and call event
+					direction = Direction.Right;
+					EventManager.InvokeEvent("TurnRight");
+				}
+				break;
+			case Direction.Right:
+				// If facing right and movement says left
+				if (horizontalMove < 0) {
+					// Change to left and call event
+					direction = Direction.Left;
+					EventManager.InvokeEvent("TurnLeft");
+				}
+				break;
+		}
+	}
+
+	private void AddEventDebug(string eventName) {
+		EventManager.AddListener(eventName, () => { Debug.Log(eventName); });
+	}
+}
