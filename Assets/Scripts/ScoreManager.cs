6e70017ce50350508fa1a60d98306292e03805a4
Abby <AbbyNode@users.noreply.github.com>
2019-11-11 19:23:53 -0500

    Basic score manager with time

---
+++ Assets/Scripts/ScoreManager.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreManager : MonoBehaviour
+{
+	public enum ScoreType { Time }
+
+	public ScoreType scoreType = ScoreType.Time;
+	public int scorePerSecond = 1;
+	public int maxScore = 60 * 10;
+
+	public int score { get; private set; }
+	public float timeSinceStart { get; private set; }
+
+	private bool trackTime;
+	private float timeStart;
+
+	// Start is called before the first frame update
+	void Start() {
+		StartTime();
+	}
+
+	// Update is called once per frame
+	void Update() {
+		if (trackTime) {
+			UpdateTime();
+		}
+	}
+
+	public void StartTime() {
+		trackTime = true;
+		timeStart = Time.time;
+	}
+
+	public void UpdateTime() {
+		float timeNow = Time.time;
+		timeSinceStart = timeNow - timeStart;
+		score = maxScore - Mathf.RoundToInt(timeSinceStart * scorePerSecond);
+	}
+
+	public void StopTime() {
+		UpdateTime();
+		trackTime = false;
+	}
+}
