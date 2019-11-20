f542073b671db47de087abf195614a46cb40ab46
Abby <AbbyNode@users.noreply.github.com>
2019-11-20 16:31:39 -0500

    Using deltaTime for gravity

--- Assets/Scripts/Entities/GravityPoints.cs
+++ Assets/Scripts/Entities/GravityPoints.cs
@@ -7,15 +7,15 @@ public class GravityPoints : MonoBehaviour
 	private EventManager eventManager;
 
     public static readonly int SMALL_USE = 1;
-    public static readonly int LARGE_USE = 1;
+    public static readonly int LARGE_USE = 2;
 
 	public int maxGravityPoints = 10;
-    public int replenishInterval = 1; // Increase points ever x seconds 
+    public int replenishInterval = 3; // Increase points ever x seconds 
     public int replenishAmount = 1; // Increase x points every interval
 
     public int gravityPoints { get; private set; }
 
-    private float timeAtLastReplenish = 0;
+    private float replenishTimer = 0;
 
     // Start is called before the first frame update
     void Start()
@@ -29,10 +29,11 @@ public class GravityPoints : MonoBehaviour
 
     void FixedUpdate()
     {
-        float timeSinceLastReplenish = Time.time - timeAtLastReplenish;
-        if (timeSinceLastReplenish >= replenishInterval && gravityPoints <= maxGravityPoints) {
+        if (replenishTimer <= 0 && gravityPoints < maxGravityPoints) {
             gravityPoints += replenishAmount;
-            timeAtLastReplenish = Time.time;
+            replenishTimer = replenishInterval;
         }
+        
+        replenishTimer -= Time.fixedDeltaTime;
     }
 }
