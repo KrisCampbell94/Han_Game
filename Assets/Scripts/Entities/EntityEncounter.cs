e863fd60d03f03072f16f7ba8c2884a1adcf1266
Abby <AbbyNode@users.noreply.github.com>
2019-12-05 16:18:56 -0500

    Minor adjustments
    Speed, attack size, etc

--- Assets/Scripts/Entities/EntityEncounter.cs
+++ Assets/Scripts/Entities/EntityEncounter.cs
@@ -18,7 +18,7 @@ public class EntityEncounter : MonoBehaviour
         {
             isEnemyClose = true;
         }
-        if (collider.gameObject.layer == 8)
+        if (collider.gameObject.layer == LayerMask.NameToLayer("Wall"))
         {
             closeToWall = true;
         }
@@ -34,7 +34,7 @@ public class EntityEncounter : MonoBehaviour
         {
             isEnemyClose = false;
         }
-        if (collider.gameObject.layer == 8)
+        if (collider.gameObject.layer == LayerMask.NameToLayer("Wall"))
         {
             closeToWall = false;
         }
