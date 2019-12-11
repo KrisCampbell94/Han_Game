65de73c320cc85545df1abc598b221aa6ba82864
Earl Denzel Perez <earldenzel@gmail.com>
2019-12-11 04:02:33 -0500

    Added FLOATING DAMAGE
    Also, projectiles no longer go through tiles

---
+++ Assets/Scripts/Physics/TileChecker.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class TileChecker : MonoBehaviour
+{
+    Tilemap tilemap;
+    // Start is called before the first frame update
+    void Start()
+    {
+        tilemap = gameObject.GetComponent<Tilemap>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }    
+    
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        if (collider.gameObject.tag == "EnemyWeapon" || collider.gameObject.tag == "PlayerWeapon")
+        {
+            collider.gameObject.SetActive(false);
+        }
+    }
+}
