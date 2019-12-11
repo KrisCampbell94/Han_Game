06c97798ff3304e5103880fb2a4e725cc6ed1ec5
Earl Denzel Perez <earldenzel@gmail.com>
2019-12-11 15:04:07 -0500

    REMOVED ALL SECRET AREAS
    - Removed all secret areas
    - Han will never go past the background tiles
    - Ensured that will all stages, Han's kunai doesn't go through tiles
    - Fixed damage visual not orienting properly
    - Fixed kick not working correctly
    - THIS IS MOST LIKELY THE FINAL BUILD FOR ARBEN

---
+++ Assets/Scripts/Physics/BoundsChecker.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoundsChecker : MonoBehaviour
+{
+    private Vector3 playerPosition;
+    private bool playerInBounds;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerPosition = Vector3.zero;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            playerInBounds = true;
+            playerPosition = collision.gameObject.transform.position;
+        } 
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            playerInBounds = false;
+            collision.gameObject.transform.position = playerPosition;
+        }
+        if (collision.gameObject.tag == "Enemy")
+        {
+            collision.GetComponent<HitPoints>().hitPoints = 0;
+        }
+
+    }
+}
